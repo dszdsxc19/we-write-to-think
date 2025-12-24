efbbae6eaedd49c495aeddd8701b084c77517a80
yong.tan7 <yong.tan7@amh-group.com>
2025-12-25 01:20:05 +0800

    feat: update ESLint configuration to ignore next.config.js and allow builds with ESLint errors

--- next.config.js
+++ next.config.js
@@ -89,6 +89,11 @@ module.exports = () => {
         },
       ]
     },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
     webpack: (config, options) => {
       config.module.rules.push({
         test: /\.svg$/,
